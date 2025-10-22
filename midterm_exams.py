515dcb8e4b66eb00def2121bd1a865c8881fac0e
CHARLIE <Janamytsko@gmail.com>
2025-10-22 13:35:05 +0100

    commit

---
+++ midterm_exams.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+names = ['Brandon', 'Vanessa', 'Daniel', 'Kevin', 'William']
+midterm = [85, 60, 60, 65, 100]
+final = [90, 90, 65, 80, 95]
+
+x = np.arange(len(names))
+width = 0.35
