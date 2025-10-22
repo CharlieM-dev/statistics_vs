256f9c4cde5ac1b3716b012631eb7cd2c2320e67
CHARLIE <Janamytsko@gmail.com>
2025-10-22 13:49:45 +0100

    Algorithm that draw  graph and estimates the change in scores between midterm exam and final exam

--- midterm_exams.py
+++ midterm_exams.py
@@ -7,3 +7,18 @@ final = [90, 90, 65, 80, 95]
 
 x = np.arange(len(names))
 width = 0.35
+
+fig, ax = plt.subplots()
+ax.bar(x - width/2, midterm, width, label='Midterm')
+ax.bar(x + width/2, final, width, label='Finals')
+
+# Label and format
+ax.set_ylabel('Score(points)')
+ax.set_title('Scores on Midterm and Final Exams')
+ax.set_xticks(x)
+ax.set_xticklabels(names)
+ax.legend()
+
+plt.show()
+
+for item2, item3 in zip(midterm, final):
