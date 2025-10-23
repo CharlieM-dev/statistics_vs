2ed3cb62e19c61b7232e4076d13dc6f5a3aecea7
CHARLIE <Janamytsko@gmail.com>
2025-10-23 15:22:27 +0100

    Now it counts how many teams are there in NFC

--- 2-waytable.py
+++ 2-waytable.py
@@ -11,3 +11,7 @@ index = ['Animal', 'Non-animal']
 
 table = pd.DataFrame(data, index=index)
 print(table)
+
+# How many teams are in NFC?
+nfc_total = table['NFC'].sum()
+print(f'In total NFC has this many teams: {nfc_total}')
